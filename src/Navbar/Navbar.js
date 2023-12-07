c6b5db8a3ce88fd8a4301ffda8fee2100593e774
Harvey1314 <haarsh1314@gmail.com>
2023-12-06 22:13:51 -0600

    14 commit

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 function Navbar(props) {
   const isActive = props.isActive;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div>
-      <nav className="bg-[#27374D] text-[#DDE6ED] w-screen">
-        <div className="flex align-middle justify-between p-5">
-          <NavLink to="/" className="text-2xl font-quicksand font-bold">
-            HARSH
-          </NavLink>
-          <ul className="flex align-middle justify-center list-none">
+    <nav className="bg-[#27374D] text-[#DDE6ED] w-screen">
+      <div className="container mx-auto flex lg:flex-row items-center justify-between p-5">
+        <NavLink to="/" className="text-2xl font-quicksand font-bold">
+          HARSH
+        </NavLink>
+        <div className="lg:hidden p-4">
+          <button
+            onClick={toggleNavbar}
+            className="text-white focus:outline-none focus:text-white"
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {!isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M19 6H5a1 1 0 000 2h14a1 1 0 100-2zM19 11H5a1 1 0 100 2h14a1 1 0 100-2zM19 16H5a1 1 0 100 2h14a1 1 0 100-2z"
+                />
+              ) : (
+                <></>
+              )}
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`lg:flex lg:flex-row lg:items-center lg:justify-between mt-4 lg:mt-0 ${
+            isOpen ? "flex flex-col items-center" : "hidden"
+          }`}
+        >
+          <ul className="flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4">
             <li className="transform translate-x-0 hover:translate-y-1 transition-transform duration-300 ease-in-out">
               <Link
                 to="/Home"
+                onClick={toggleNavbar}
                 className={`px-8 font-quicksand hover:shadow-custom rounded-3xl py-2 ${
                   isActive === "1"
                     ? "py-2 text-[#27374D] bg-[#DDE6ED] rounded-3xl font-bold"
@@ -26,6 +58,7 @@ function Navbar(props) {
             <li className="transform translate-x-0 hover:translate-y-1 transition-transform duration-300 ease-in-out">
               <Link
                 to="/Projects"
+                onClick={toggleNavbar}
                 className={`px-8 font-quicksand hover:shadow-custom rounded-3xl py-2 ${
                   isActive === "2"
                     ? "py-2 text-[#27374D] bg-[#DDE6ED] rounded-3xl font-bold"
@@ -38,6 +71,7 @@ function Navbar(props) {
             <li className="transform translate-x-0 hover:translate-y-1 transition-transform duration-300 ease-in-out">
               <Link
                 to="/Contact"
+                onClick={toggleNavbar}
                 className={`px-8 font-quicksand hover:shadow-custom rounded-3xl py-2 ${
                   isActive === "3"
                     ? "py-2 text-[#27374D] bg-[#DDE6ED] rounded-3xl font-bold"
@@ -49,8 +83,8 @@ function Navbar(props) {
             </li>
           </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
