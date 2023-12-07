e72dcc4894dda8e17ad9e13e6d7f88da58df59ee
Harvey1314 <haarsh1314@gmail.com>
2023-12-06 21:26:39 -0600

    11 commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import "./App.css";
 import Home from "./Home/Home.js";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <BrowserRouter>
+        <Router>
           <Routes>
-            <Route path="/HarshPortfolioReact/" element={<Home />} />
-            <Route path="/HarshPortfolioReact/Home" element={<Home />} />
-            <Route
-              path="/HarshPortfolioReact/Projects"
-              element={<Projects />}
-            />
-            <Route path="/HarshPortfolioReact/Contact" element={<Contact />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Contact" element={<Contact />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </div>
     </div>
   );
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,16 +6,13 @@ function Navbar(props) {
     <div>
       <nav className="bg-[#27374D] text-[#DDE6ED] w-screen">
         <div className="flex align-middle justify-between p-5">
-          <a
-            href="/HarshPortfolioReact/Home"
-            className="text-2xl font-quicksand font-bold"
-          >
+          <a href="/Home" className="text-2xl font-quicksand font-bold">
             HARSH
           </a>
           <ul className="flex align-middle justify-center list-none">
             <li className="transform translate-x-0 hover:translate-y-1 transition-transform duration-300 ease-in-out">
               <a
-                href="/HarshPortfolioReact/Home"
+                href="/Home"
                 className={`px-8 font-quicksand hover:shadow-custom rounded-3xl py-2 ${
                   isActive === "1"
                     ? "py-2 text-[#27374D] bg-[#DDE6ED] rounded-3xl font-bold"
@@ -27,7 +24,7 @@ function Navbar(props) {
             </li>
             <li className="transform translate-x-0 hover:translate-y-1 transition-transform duration-300 ease-in-out">
               <a
-                href="/HarshPortfolioReact/Projects"
+                href="/Projects"
                 className={`px-8 font-quicksand hover:shadow-custom rounded-3xl py-2 ${
                   isActive === "2"
                     ? "py-2 text-[#27374D] bg-[#DDE6ED] rounded-3xl font-bold"
@@ -39,7 +36,7 @@ function Navbar(props) {
             </li>
             <li className="transform translate-x-0 hover:translate-y-1 transition-transform duration-300 ease-in-out">
               <a
-                href="/HarshPortfolioReact/Contact"
+                href="/Contact"
                 className={`px-8 font-quicksand hover:shadow-custom rounded-3xl py-2 ${
                   isActive === "3"
                     ? "py-2 text-[#27374D] bg-[#DDE6ED] rounded-3xl font-bold"
